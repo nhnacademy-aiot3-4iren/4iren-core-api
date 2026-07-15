47fb1b8e51dc418d62b5f897dd7b17f98f9e343a
yeon <yoouu72@gmail.com>
2026-07-16 08:59:08 +0900

    refactor: Building, Device, Room, Sensor Repository 추가

--- src/main/java/com/nhnacademy/core/repository/DeviceRepository.java
+++ src/main/java/com/nhnacademy/core/repository/DeviceRepository.java
@@ -2,9 +2,17 @@ package com.nhnacademy.core.repository;
 
 import com.nhnacademy.core.domain.Device;
 import com.nhnacademy.core.domain.Room;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DeviceRepository extends JpaRepository<Device, Long> {
+
+    Page<Device> findAllByRoom_Building_TeamId(Long teamId, Pageable pageable);
+
+    Optional<Device> findByIdAndRoom_Building_TeamId(Long deviceId, Long teamId);
 
     boolean existsByRoom(Room room);
 }
--- src/main/java/com/nhnacademy/core/repository/SensorRepository.java
+++ src/main/java/com/nhnacademy/core/repository/SensorRepository.java
@@ -2,9 +2,24 @@ package com.nhnacademy.core.repository;
 
 import com.nhnacademy.core.domain.Room;
 import com.nhnacademy.core.domain.Sensor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SensorRepository extends JpaRepository<Sensor, Long> {
+
+    Page<Sensor> findAllByRoom_Building_TeamId(Long teamId, Pageable pageable);
+
+    List<Sensor> findAllByRoom_Building_TeamId(Long teamId);
+
+    List<Sensor> findAllByRoom_IdAndRoom_Building_TeamId(Long roomId, Long teamId);
+
+    Optional<Sensor> findByIdAndRoom_Building_TeamId(Long sensorId, Long teamId);
+
+    Optional<Sensor> findByDevEui(String devEui);
 
     boolean existsByRoom(Room room);
 
