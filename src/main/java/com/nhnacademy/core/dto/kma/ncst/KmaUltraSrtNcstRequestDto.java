7e5ea64ea66872fe1ab3259002513d0b77bcbb19
yooncy123 <nightbang21@gmail.com>
2026-07-16 14:04:52 +0900

    fix(kma): kma api 호출 에러 수정 및 api 엔드포인트 수정
    Fixes #23

--- src/main/java/com/nhnacademy/core/dto/kma/ncst/KmaUltraSrtNcstRequestDto.java
+++ src/main/java/com/nhnacademy/core/dto/kma/ncst/KmaUltraSrtNcstRequestDto.java
@@ -1,16 +1,14 @@
 package com.nhnacademy.core.dto.kma.ncst;
 
-/**
- * 초단기실황조회 요청 DTO
- * @param base_date
- * @param base_time
- * @param nx
- * @param ny
- */
-public record KmaUltraSrtNcstRequestDto(
-        String base_date,
-        String base_time,
-        Integer nx,
-        Integer ny
-) {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class KmaUltraSrtNcstRequestDto {
+
+    private final String base_date;
+    private final String base_time;
+    private final Integer nx;
+    private final Integer ny;
 }
