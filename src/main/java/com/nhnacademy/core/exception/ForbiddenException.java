2b832e7a15c4b2c03e62a1a151b08b5b0c143029
yeon <yoouu72@gmail.com>
2026-08-26 09:11:43 +0900

    Reapply "refactor: 예외 클래스 편의 생성자 추가"
    This reverts commit 1d20188aaef98dfccfa2e6b488e516a096a0ddc3.

--- src/main/java/com/nhnacademy/core/exception/ForbiddenException.java
+++ src/main/java/com/nhnacademy/core/exception/ForbiddenException.java
@@ -10,8 +10,23 @@ public class ForbiddenException extends ApplicationException {
 
     public ForbiddenException(
             ErrorCode errorCode,
+            Throwable cause
+    ) {
+        super(errorCode, cause);
+    }
+
+    public ForbiddenException(
+            ErrorCode errorCode,
             Map<String, Object> context
     ) {
         super(errorCode, context);
+    }
+
+    public ForbiddenException(
+            ErrorCode errorCode,
+            Map<String, Object> context,
+            Throwable cause
+    ) {
+        super(errorCode, context, cause);
     }
 }
